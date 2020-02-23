ec2d46c99d5e6bd0868eb6c420df4d8d451efe5b
SteveSmokov <steve.smokov@ukr.net>
2020-02-24 00:41:32 +0200

    ДЗ GC

--- hw03-memory/src/main/java/ru/otus/Main.java
+++ hw03-memory/src/main/java/ru/otus/Main.java
@@ -1,7 +1,41 @@
 package ru.otus;
 
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Start process ID=" + ManagementFactory.getRuntimeMXBean().getName());
+        switchOnMonitoring();
 
+        System.out.println("Stop process!");
+    }
+
+    private static void switchOnMonitoring() {
+        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcbean : gcbeans) {
+            System.out.println("GC name:" + gcbean.getName());
+            NotificationEmitter emitter = (NotificationEmitter) gcbean;
+            NotificationListener listener = (notification, handback) -> {
+                if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from((CompositeData) notification.getUserData());
+                    String gcName = info.getGcName();
+                    String gcAction = info.getGcAction();
+                    String gcCause = info.getGcCause();
+
+                    long startTime = info.getGcInfo().getStartTime();
+                    long duration = info.getGcInfo().getDuration();
+
+                    System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
+                }
+            };
+            emitter.addNotificationListener(listener, null, null);
+        }
     }
 }
